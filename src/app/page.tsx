3e9395e8ab433c155b4115c2c0c59c7c001dbdb2
Matheus Altrão <matheusaltrao2@gmail.com>
2023-12-26 17:10:17 -0400

    feat: create hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,34 @@
+import Image from 'next/image';
+import HeroImage from '../assets/heroImage.svg';
+
 export default function Home() {
-    return <div className='min-h-screen '></div>;
+    return (
+        <div className=' mt-32 w-full max-w-[900px] mx-auto flex items-center justify-center flex-col gap-8 '>
+            <div className=' pr-8 '>
+                <Image
+                    src={HeroImage}
+                    alt=''
+                    height={400}
+                    width={400}
+                    priority={true}
+                    className='w-full max-w-[400px] max-h-[400px]'
+                />
+            </div>
+
+            <div className='flex flex-col items-center justify-center gap-4 w-full text-center'>
+                <h1 className='text-2xl font-semibold text-center max-w-[300px]'>
+                    Sistema feito para você organizar seus estudos e terefas
+                </h1>
+            </div>
+
+            <div className='flex items-center flex-col sm:flex-row justify-center gap-2'>
+                <div className='card text-center py-2 w-[180px] text-sm'>
+                    + 7mil posts
+                </div>
+                <div className='card text-center py-2 w-[180px] text-sm'>
+                    + 2 mil comentários
+                </div>
+            </div>
+        </div>
+    );
 }
